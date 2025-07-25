8bbf60cd4d3928dbbee336c923a5654048bfe3f9
Diego Giorgini <diego.giorgini@icloud.com>
2025-07-25 08:23:21 +0200

    add huggingface builder

--- src/builders/__init__.py
+++ src/builders/__init__.py
@@ -1,4 +1,5 @@
 from .code_repo import CodeRepoBuilder, CodeRepoResult
+from .huggingface import HFBuilder, HFResult
 from .linkedin import LinkedinBuilder, LinkedinResult
 from .website import WebsiteBuilder, PageResult
 from .x import XBuilder, XResult
