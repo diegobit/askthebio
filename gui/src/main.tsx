f59a0d150d4bf3158905ed7bd7795d7eda116554
Diego Giorgini <diegobit@icloud.com>
2025-10-27 22:34:03 +0100

    gui: support dark mode

--- gui/src/main.tsx
+++ gui/src/main.tsx
@@ -10,9 +10,23 @@ const setMetaContent = (selector: string, content: string) => {
   }
 };
 
+const applySystemTheme = (mq: MediaQueryList) => {
+  document.documentElement.classList.toggle('dark', mq.matches);
+};
+
 document.title = appConfig.title;
 setMetaContent('meta[name="description"]', appConfig.description);
 setMetaContent('meta[property="og:title"]', appConfig.title);
 setMetaContent('meta[property="og:description"]', appConfig.description);
+
+const systemThemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+applySystemTheme(systemThemeQuery);
+
+const listener = (event: MediaQueryListEvent) => applySystemTheme(event);
+if (typeof systemThemeQuery.addEventListener === 'function') {
+  systemThemeQuery.addEventListener('change', listener);
+} else {
+  systemThemeQuery.addListener(listener); // Safari < 14
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
