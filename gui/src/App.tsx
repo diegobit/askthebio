f5fb54f40902c0eb8bf2eb4f3aec5913af9887e3
Diego Giorgini <diegobit@icloud.com>
2025-10-30 18:35:08 +0100

    gui: add corner badge with links

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -2,11 +2,22 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { CornerInfoBadge } from "@/components/CornerBadge";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+
+const HomeBadges = () => {
+  const location = useLocation();
+
+  if (location.pathname !== "/") {
+    return null;
+  }
+
+  return <CornerInfoBadge />;
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -19,6 +30,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <HomeBadges />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
